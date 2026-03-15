d4e047d8668bf883840a92ca5aad10936a241db1
Deepanshu2501 <tomarvashu445@gmail.com>
2026-03-15 18:06:56 +0530

    updated some files and added images

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser } from  "../controllers/user.controller.js"
+import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+// to inject middleware, in which method we want to use, add just before it
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 export default router  
